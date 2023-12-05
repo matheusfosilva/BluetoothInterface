08b740e575655a5c4ae088cfff8dd311d9681687
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2023-12-04 23:31:05 -0300

    test

--- App.js
+++ App.js
@@ -1,38 +1,193 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from "react";
 import {
   SafeAreaView,
   StyleSheet,
   View,
   Image,
   TouchableOpacity,
-  Text
-} from 'react-native';
-import App from "react-native-ble-manager/example/App";
+  Text,
+  Platform,
+  PermissionsAndroid,
+} from "react-native";
+import { BleManager } from "react-native-ble-plx";
 
-import right from "./assets/rightButton.png"
-import left from "./assets/leftButton.png"
-import front from "./assets/frontButton.png"
-import back from "./assets/backButton.png"
-import icon from "./assets/justDog.png"
+import right from "./assets/rightButton.png";
+import left from "./assets/leftButton.png";
+import front from "./assets/frontButton.png";
+import back from "./assets/backButton.png";
+import icon from "./assets/justDog.png";
 
+const manager = new BleManager();
+
+const ConnectionStatus = {
+  DISCONNECTED: "desconectado",
+  CONNECTING: "conectando",
+  CONNECTED: "conectado",
+};
+
+const requestBluetoothPermission = async () => {
+  if (
+    Platform.OS === "android" &&
+    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+  ) {
+    const apiLevel = parseInt(Platform.Version.toString(), 10);
+
+    if (apiLevel < 31) {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    }
+    if (
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN &&
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+    ) {
+      const result = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ]);
+
+      return (
+        result["android.permission.BLUETOOTH_CONNECT"] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+        result["android.permission.BLUETOOTH_SCAN"] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+        result["android.permission.ACCESS_FINE_LOCATION"] ===
+          PermissionsAndroid.RESULTS.GRANTED
+      );
+    }
+  }
+
+  this.showErrorToast("Permission have not been granted");
+
+  return false;
+};
 
 export default function App() {
+  const [connectionStatus, setConnectionStatus] = React.useState(
+    ConnectionStatus.DISCONNECTED
+  );
+
+  function scanAndConnect() {
+    manager.startDeviceScan(null, null, async (error, device) => {
+      setConnectionStatus(ConnectionStatus.CONNECTING);
+      if (error) {
+        // Handle error (scanning will be stopped automatically)
+        return;
+      }
+
+      // Check if it is a device you are looking for based on advertisement data
+      console.log("🚀 ~ manager.startDeviceScan ~ device:", device.name);
+      // or other criteria.
+      if (device.name === "Gamesir-X2_EF") {
+        // Stop scanning as it's not necessary if you are scanning for one device.
+        manager.stopDeviceScan();
+
+        // Proceed with connection on Gamesir-X2_EF device.
+        try {
+          await manager.connectToDevice(device.id);
+
+          await manager.discoverAllServicesAndCharacteristicsForDevice(
+            device.id
+          );
+        } catch (err) {
+          console.log("🚀 ~ manager.startDeviceScan ~ err:", err);
+        }
+        setConnectionStatus(ConnectionStatus.CONNECTED);
+      }
+    });
+  }
+
+  React.useEffect(() => {
+    const requestBluetoothPermission = async () => {
+      if (
+        Platform.OS === "android" &&
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      ) {
+        const apiLevel = parseInt(Platform.Version.toString(), 10);
+
+        if (apiLevel < 31) {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+          );
+          return granted === PermissionsAndroid.RESULTS.GRANTED;
+        }
+        if (
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN &&
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+        ) {
+          const result = await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          ]);
+
+          return (
+            result["android.permission.BLUETOOTH_CONNECT"] ===
+              PermissionsAndroid.RESULTS.GRANTED &&
+            result["android.permission.BLUETOOTH_SCAN"] ===
+              PermissionsAndroid.RESULTS.GRANTED &&
+            result["android.permission.ACCESS_FINE_LOCATION"] ===
+              PermissionsAndroid.RESULTS.GRANTED
+          );
+        }
+      }
+
+      showErrorToast("Permission have not been granted");
+
+      return false;
+    };
+
+    const fetchData = async () => {
+      let result;
+      try {
+        result = await requestBluetoothPermission();
+      } catch (error) {
+        console.log("🚀 ~ React.useEffect ~ error", error);
+      }
+      console.log("🚀 ~ React.useEffect ~ result:", result);
+      if (result) {
+        scanAndConnect();
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      
-        <View style={styles.header}>
-          <Image style={styles.headerIcon} source={icon} />
-          <Text style={styles.headerText}>MotorPet</Text>
-        </View>
-     
-      <View style={styles.principalView}>
+      <View style={styles.header}>
+        <Image style={styles.headerIcon} source={icon} />
+        <Text style={styles.headerText}>MotorPet</Text>
+      </View>
 
+      <View style={styles.connectionStatus}>
+        <Text
+          style={{
+            color: "black",
+            fontSize: 18,
+            fontWeight: "bold",
+          }}
+        >
+          Status do dispositivo:{" "}
+          <Text
+            style={{
+              color: "black",
+              fontSize: 18,
+              fontWeight: "normal",
+            }}
+          >
+            {connectionStatus}
+          </Text>
+        </Text>
+      </View>
+      <View style={styles.principalView}>
         <TouchableOpacity>
           <Image style={styles.button} source={front} />
         </TouchableOpacity>
 
         <View style={styles.aroundView}>
-
           <TouchableOpacity>
             <Image style={styles.button} source={left} />
           </TouchableOpacity>
@@ -40,15 +195,12 @@ export default function App() {
           <TouchableOpacity>
             <Image style={styles.button} source={right} />
           </TouchableOpacity>
-
         </View>
 
         <TouchableOpacity>
           <Image style={styles.button} source={back} />
         </TouchableOpacity>
-
       </View>
-
     </SafeAreaView>
   );
 }
@@ -56,44 +208,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5a142'
+    backgroundColor: "#f5a142",
   },
   header: {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    height: '12%',
-    backgroundColor: 'black',
-    alignItems: 'flex-end',
-    paddingBottom: 10
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    height: "12%",
+    backgroundColor: "black",
+    alignItems: "flex-end",
+    paddingBottom: 10,
   },
   headerText: {
-    color: '#f5a142',
+    color: "#f5a142",
     fontSize: 40,
-    fontWeight: '900',
+    fontWeight: "900",
     paddingLeft: 15,
-    paddingTop: 30
+    paddingTop: 30,
   },
   headerIcon: {
     width: 100,
-    height: 100
+    height: 100,
   },
   principalView: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    gap: 15
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    gap: 15,
   },
   aroundView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row'
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
   },
   button: {
     width: 100,
     height: 100,
     marginLeft: 55,
-    marginRight: 45
-  }
+    marginRight: 45,
+  },
+  connectionStatus: {
+    width: "100%",
+    marginVertical: 10,
+    alignItems: "center",
+  },
 });
